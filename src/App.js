d975b456fd0b19d98989f623aee4200bb587f357
Anik Bhattacharya <anikbhattachar27@gmail.com>
2023-08-12 14:35:03 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,39 @@ import { useEffect,useState } from "react";
 import { weatherData } from "./weatherApi";
 function App() {
 
+  const[city, setCity] =useState("Delhi");
   const [weather,setWeather] = useState(null);
+  const [units,setUnits] = useState("metric");
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchWeatherData = async()=>{
-      const data = await weatherData("paris")
-      setWeather(data);
-    }
+      setError(false); // Reset error state before fetching new data
 
+      try {
+        const data = await weatherData(city, units);
+        setWeather(data);
+      } catch (error) {
+        setError(true); // Set error state if API call fails
+      }
+    };
     fetchWeatherData();
-  },[])
+  },[units,city])
+
+  const handleUnitsClick = (e) => {
+    const button = e.currentTarget;
+    const currentUnit = button.innerText.slice(1);
+    
+    const isCelcius = currentUnit === 'C';
+    button.innerText = isCelcius ? '°F' : '°C'
+    setUnits(isCelcius ? 'metric' : 'imperial');
+  }
+
+  const enterKeyPressed = (e) =>{
+    if(e.keyCode === 13){
+      setCity(e.currentTarget.value);
+      e.currentTarget.blur();
+    }
+  }
   return (
     <div className="App">
       <div className="overlay">
@@ -22,8 +46,10 @@ function App() {
           weather && (
             <div className="container">
             <div className="section section_inputs">
-              <input type="text" name="city" placeholder="city" />
-              <button>℉</button>
+              <input
+              onKeyDown={enterKeyPressed} 
+              type="text" name="city" placeholder="city" />
+              <button onClick={(e)=>handleUnitsClick(e)}>℉</button>
             </div>
             <div className="section section_temparature">
               <div className="icon">
@@ -35,10 +61,10 @@ function App() {
                 <h3>{weather.description}</h3>
               </div>
               <div className="temperature1">
-                <h1>{`${weather.temp.toFixed()} ℃`}</h1>
+                <h1>{`${weather.temp.toFixed()} °${units === 'metric'?'C':'F'}`}</h1>
               </div>
             </div>
-            <Description weather={weather}/>
+            <Description weather={weather} units={units}/>
           </div>
           )
         }
